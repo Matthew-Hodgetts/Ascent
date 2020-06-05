26d4a4e6bfe09253734abd2a0c92870f7c7ec46f
Matthew Hodgetts <matt.hodgetts@outlook.com>
2020-06-05 15:33:11 +0100

    Button styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
-import React from "react"
-import Layout from "@common/Layout/Layout"
-import SEO from "@common/Seo/Seo"
+import React from "react";
+import Layout from "@common/Layout/Layout";
+import SEO from "@common/Seo/Seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div className="container">
+      <div className="row">
+        <div className="col"></div>
+      </div>
+    </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
